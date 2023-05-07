b1fe6f109d0c9c1b23f28f05c86a1ab28807c4e9
Umama Masroor <umamafatima319@gmail.com>
2023-05-06 23:01:24 -0700

    question solve

---
+++ question # 5/add.js
@@ -0,0 +1,3 @@
+// question 5 
+let variable = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+alert(variable);
---
+++ question # 6/add.js
@@ -0,0 +1,7 @@
+// question 6
+// Declare a variable called email and assign to it a string that 
+// represents your Email Address(e.g. example@example.com). 
+// Show the blow mentioned message in an alert box.(Hint: use 
+// string concatenation)
+let email = " umamamasroor876@gmail.com"
+alert("My email address is" + email);
