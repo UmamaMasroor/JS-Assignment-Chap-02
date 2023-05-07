22c02116aebbbe29e8ab9be803d4d49cea118791
Umama Masroor <umamafatima319@gmail.com>
2023-05-06 23:07:00 -0700

    question 7

---
+++ question # 7/add.js
@@ -0,0 +1,6 @@
+// question 7
+// Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+let message = " I am trying to learn from a book A Smarter\n Way to Learn JavaScript";
+alert (message);
