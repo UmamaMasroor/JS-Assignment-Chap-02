f4f8d732ed8d6517ae74bb20a589d29e5fb695a7
Umama Masroor <umamafatima319@gmail.com>
2023-05-06 22:49:06 -0700

    question 4

---
+++ question # 4/add.js
@@ -0,0 +1,11 @@
+// question 4
+// Write a script to save student’s bio data in JS variables and 
+// show the data in alert boxes
+let name = "Ahmed";
+alert(name);
+let age = "15";
+alert(age);
+let gender = "male";
+alert(gender);
+let profession = "Certified Web and App Developer";
+alert(profession);
