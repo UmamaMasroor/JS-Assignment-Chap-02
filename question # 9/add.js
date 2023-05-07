d5a4857821e6e047ddce2e0d8d528c34b2c30968
Umama Masroor <umamafatima319@gmail.com>
2023-05-06 23:28:10 -0700

    question # 9

---
+++ question # 9/add.js
@@ -0,0 +1,7 @@
+// question 9
+//Store following string in a variable and show in alert and 
+// browser through JS
+// “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+let string = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert (string);
+document.write("<p>" +string + "</p>");
