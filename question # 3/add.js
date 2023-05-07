b56f2c47fad9bb58356d4f257772053173830e86
Umama Masroor <umamafatima319@gmail.com>
2023-05-06 22:38:55 -0700

    question 3

---
+++ question # 3/add.js
@@ -0,0 +1,8 @@
+// question 3
+// Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+let message ="Hello World";
+alert(message);
